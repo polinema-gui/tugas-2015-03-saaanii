bd734c9fe5cf84923f762c394af00d5ac7138f66
saaanii <saniwisnu140@gmail.com>
2015-10-17 18:49:38 +0700

    Revert "1431140150-tugas-2015-03"
    This reverts commit f8d86411f0824be9292be1536b7895932a257bd6.

--- Perulangan/Form3.Designer.vb
+++ Perulangan/Form3.Designer.vb
@@ -22,138 +22,19 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Txb_matrik1 = New System.Windows.Forms.TextBox()
-        Me.Txb_matrik2 = New System.Windows.Forms.TextBox()
-        Me.Cmb_operasi = New System.Windows.Forms.ComboBox()
-        Me.Txb_hasil = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Btn_proses = New System.Windows.Forms.Button()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(14, 41)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(41, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "matrik1"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(14, 154)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(41, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "matrik2"
-        '
-        'Txb_matrik1
-        '
-        Me.Txb_matrik1.Location = New System.Drawing.Point(63, 18)
-        Me.Txb_matrik1.Multiline = True
-        Me.Txb_matrik1.Name = "Txb_matrik1"
-        Me.Txb_matrik1.Size = New System.Drawing.Size(313, 64)
-        Me.Txb_matrik1.TabIndex = 2
-        '
-        'Txb_matrik2
-        '
-        Me.Txb_matrik2.Location = New System.Drawing.Point(63, 127)
-        Me.Txb_matrik2.Multiline = True
-        Me.Txb_matrik2.Name = "Txb_matrik2"
-        Me.Txb_matrik2.Size = New System.Drawing.Size(313, 64)
-        Me.Txb_matrik2.TabIndex = 3
-        '
-        'Cmb_operasi
-        '
-        Me.Cmb_operasi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.Cmb_operasi.FormattingEnabled = True
-        Me.Cmb_operasi.Items.AddRange(New Object() {"Tambah", "Kurang"})
-        Me.Cmb_operasi.Location = New System.Drawing.Point(68, 94)
-        Me.Cmb_operasi.Name = "Cmb_operasi"
-        Me.Cmb_operasi.Size = New System.Drawing.Size(307, 21)
-        Me.Cmb_operasi.TabIndex = 5
-        '
-        'Txb_hasil
-        '
-        Me.Txb_hasil.Location = New System.Drawing.Point(62, 206)
-        Me.Txb_hasil.Multiline = True
-        Me.Txb_hasil.Name = "Txb_hasil"
-        Me.Txb_hasil.Size = New System.Drawing.Size(313, 124)
-        Me.Txb_hasil.TabIndex = 6
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(14, 262)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(30, 13)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Hasil"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(53, 365)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(71, 20)
-        Me.Button1.TabIndex = 8
-        Me.Button1.Text = "Keluar"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Btn_proses
-        '
-        Me.Btn_proses.Location = New System.Drawing.Point(284, 365)
-        Me.Btn_proses.Name = "Btn_proses"
-        Me.Btn_proses.Size = New System.Drawing.Size(71, 20)
-        Me.Btn_proses.TabIndex = 9
-        Me.Btn_proses.Text = "Proses"
-        Me.Btn_proses.UseVisualStyleBackColor = True
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(14, 97)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(41, 13)
-        Me.Label4.TabIndex = 10
-        Me.Label4.Text = "operasi"
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(399, 402)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Btn_proses)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Txb_hasil)
-        Me.Controls.Add(Me.Cmb_operasi)
-        Me.Controls.Add(Me.Txb_matrik2)
-        Me.Controls.Add(Me.Txb_matrik1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(441, 402)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form3"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Matriks Kalkulator"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Txb_matrik1 As System.Windows.Forms.TextBox
-    Friend WithEvents Txb_matrik2 As System.Windows.Forms.TextBox
-    Friend WithEvents Cmb_operasi As System.Windows.Forms.ComboBox
-    Friend WithEvents Txb_hasil As System.Windows.Forms.TextBox
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Btn_proses As System.Windows.Forms.Button
-    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
