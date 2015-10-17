f85721d4a5916f5e8f45278496d998d2f1824e30
saaanii <saniwisnu140@gmail.com>
2015-10-17 18:10:01 +0700

    Revert "Tugas Perulangan"
    This reverts commit a386513715ef08a43e49118638be2229301c4942.

--- Perulangan/Form1.vb
+++ Perulangan/Form1.vb
@@ -1,20 +1,3 @@
 ﻿Public Class Form1
 
-    Private Sub AplikasiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AplikasiToolStripMenuItem.Click
-
-    End Sub
-
-    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintangToolStripMenuItem.Click
-        bintang.MdiParent = Me
-        bintang.Show()
-    End Sub
-
-    Private Sub MatriksKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MatriksKalkulatorToolStripMenuItem.Click
-        matriks.MdiParent = Me
-        matriks.Show()
-    End Sub
-
-    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
-        Close()
-    End Sub
 End Class
